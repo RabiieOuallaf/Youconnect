32ef11dc731f32be85ebe3d8227db308f090a389
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2022-12-09 22:33:26 +0100

    Setting up redux

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -1,1 +1,38 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    mode : "light",
+    user: null,
+    token: null,
+    posts: [],
+};
+
+export const authSlice = () => {
+
+    name: "auth",
+    initialState,
+    reducers : {
+        setMode: (state) => {
+            state.mode = state.mode === "light" ? "dark" : "light";
+        }
+
+        setLogin: (state, action) => {
+            state.user = action.payload.user;
+            state.token = action.payload.token;
+        }
+
+        setLogout: (state) => {
+            state.user = null;
+            state.token = null;
+        }
+
+        setFriends: (state, action) => {
+            if(state.user){
+                state.user.friends = action.payload.friends;
+            }else {
+                console.log("user friends does not exsit");
+            }
+        }
+    }
+
+};
