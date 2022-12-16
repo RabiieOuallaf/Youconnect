fa69e758f4dff2b790971018d3fe889d40694f66
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2022-12-16 21:51:07 +0100

    Setting MUI provider

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,36 @@ import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 import HomePage from './scenes/HomePage';
 import LoginPage from './scenes/LoginPage';
 import ProfilePage from './scenes/ProfilePage';
-
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { CssBaseline , ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from './theme';
+import { create } from 'yup/lib/Reference';
 
 function App() {
+
+  const mode = useSelector( (state) => state.mode);
+  const theme = useMemo( () =>  createTheme(themeSettings(mode)) , [mode]);
+
   return (
     <div className='app'>
       <BrowserRouter>
-        <Routes>
 
-          <Route path='/' element={<LoginPage />} />
-          <Route path='/home' element={<HomePage />}/>
-          <Route path='/profile/:userId' element={<ProfilePage />}/>
-        </Routes>
+        <ThemeProvider theme={theme}>
+
+          <CssBaseline />
+
+            <Routes>
+
+              <Route path='/' element={<LoginPage />} />
+              <Route path='/home' element={<HomePage />}/>
+              <Route path='/profile/:userId' element={<ProfilePage />}/>
+
+            </Routes>
+
+        </ThemeProvider>
+        
       </BrowserRouter>
     </div>
   );
