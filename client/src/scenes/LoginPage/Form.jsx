c9c1a971885f58cb864fa5ad384d400dd32cd68a
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2023-01-18 20:23:23 +0100

    starting the for

--- client/src/scenes/LoginPage/Form.jsx
+++ client/src/scenes/LoginPage/Form.jsx
@@ -32,6 +32,7 @@ const initialValueRegister = {
     email: "",
     password: "",
     location: "",
+    occupation:"",
     company: "",
     picture: ""
 }
@@ -74,7 +75,7 @@ const Form = () => {
                     <Box
                         display="grid"
                         gap="30px"
-                        gridTemplateColumns="repeat(4, minxmax(0,1fr))"
+                        gridTemplateColumns="repeat(4, minmax(0,1fr))"
                         sx={{
                             "& > div" : {gridColumn: isNonMobile? undefined : 'span 4'}
                         }}
@@ -86,7 +87,103 @@ const Form = () => {
                                     onBlur={handleBlur}
                                     onChange={handleChange}
                                     value={values.firstName}
+                                    name="firstName"
+                                    error={Boolean(touched.firstName) && Boolean(errors.firstName)}
+                                    helperText={touched.firstName && errors.firstName}
+                                    sx={
+                                        {
+                                            gridColumn: "span 2"
+                                        }
+                                    }
                                 />
+
+                                <textField 
+                                    label="Last name"
+                                    onBlur={handleBlur}
+                                    onChange={handleChange}
+                                    value={values.lastName}
+                                    name="lastnName"
+                                    error={Boolean(touched.lastName) && Boolean(errors.lastName)}
+                                    helperText={touched.lastName && errors.lastName}
+                                    sx={
+                                        {
+                                            gridColumn: "span 2"
+                                        }
+                                    }
+                                />
+
+                                <textField 
+                                    label="Location"
+                                    onBlur={handleBlur}
+                                    onChange={handleChange}
+                                    value={values.location}
+                                    name="location"
+                                    error={Boolean(touched.location) && Boolean(errors.location)}
+                                    helperText={touched.location && errors.location}
+                                    sx={
+                                        {
+                                            gridColumn: "span 2"
+                                        }
+                                    }
+                                />
+
+                                <textField 
+                                    label="occupation"
+                                    onBlur={handleBlur}
+                                    onChange={handleChange}
+                                    value={values.occupation}
+                                    name="occupation"
+                                    error={Boolean(touched.occupation) && Boolean(errors.occupation)}
+                                    helperText={touched.occupation && errors.occupation}
+                                    sx={
+                                        {
+                                            gridColumn: "span 2"
+                                        }
+                                    }
+                                />
+                                <Box
+                                    gridColumns="span 4"
+                                    border={`1px solid ${pallete.neutral.medium}`}
+                                    borderRadius="5px"
+                                    p="1rem"
+                                >
+
+                                    <Dropzone
+                                        acceptedFiles=".jpg,.jpeg,.png"
+                                        multiple={false}
+                                        onDrop={(acceptedFiles) => 
+                                            setFieldValue("picture", acceptedFiles[0])
+                                        }
+                                    >
+
+                                        {({ getRootProps, getInputProps }) => (
+                                            <Box
+                                                {...getRootProps()}
+                                                border={`2px dashed ${pallete.primary.main}`}
+                                                p="1rem"
+                                                sx={{ "&:hover": {cursor:pointer} }}
+                                            >
+
+                                                <input {...getInputProps()} />
+                                                {!values.picture ? (
+
+                                                    <p>Add a picture here</p>
+                                
+
+                                                ) : (
+                                                    <JustifyBetween>
+
+                                                        
+
+                                                    </JustifyBetween>
+                                                )}
+
+                                            </Box>
+                                        )}
+
+                                    </Dropzone>
+
+                                </Box>
                             </>
                         )}
 
